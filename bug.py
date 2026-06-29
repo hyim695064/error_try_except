a65d477e9c6d83b651b5ad5eea51f04a00d217e6
moshe <hyim695064@gmail.com>
2026-06-29 16:16:13 +0300

    part1s3

--- bug.py
+++ bug.py
@@ -6,9 +6,16 @@
 # except ValueError:
 #     print("except error")
 
-a = int(input("first number:"))
-b = int(input("second number:"))
+# a = int(input("first number:"))
+# b = int(input("second number:"))
+# try:
+#     print(a/b)
+# except ZeroDivisionError:
+#     print("dont divide by zero")
+
+numbers = [10, 20, 30]
+index = int(input("choose index:"))
 try:
-    print(a/b)
-except ZeroDivisionError:
-    print("dont divide by zero")
+    print(numbers[index])
+except IndexError:
+    print("index not found")
