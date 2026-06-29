6673bcf075bb51473fab89ecc3106bd507ba902b
moshe <hyim695064@gmail.com>
2026-06-29 16:08:26 +0300

    part1s2

--- bug.py
+++ bug.py
@@ -1,7 +1,14 @@
-age = input("enter your age:")
-try:
-    next_year = int(age) + 1
+# age = input("enter your age:")
+# try:
+#     next_year = int(age) + 1
 
-    print("next year you will be", next_year)
-except ValueError:
-    print("except error")
+#     print("next year you will be", next_year)
+# except ValueError:
+#     print("except error")
+
+a = int(input("first number:"))
+b = int(input("second number:"))
+try:
+    print(a/b)
+except ZeroDivisionError:
+    print("dont divide by zero")
